2bdc0812ec8f8b51c77a95410315115dcf1fa480
silviawolves <silviacanepa.w@gmail.com>
2022-11-24 11:39:44 +0100

    clean api call url

--- src/api/weather.jsx
+++ src/api/weather.jsx
@@ -2,23 +2,17 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 import {API_KEY} from './api_key';
 
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/';
+const myHeader = `&appid=${API_KEY}&units=metric`;
 
 export const weatherApi = createApi({
     reducerPath: 'weatherApi',
-    baseQuery: fetchBaseQuery({
-        baseUrl: BASE_URL,
-        // baseUrl: `${BASE_URL}&appid=${API_KEY}&units=metric`,
-    }),
+    baseQuery: fetchBaseQuery({baseUrl: BASE_URL}),
     endpoints: (builder) => ({
         getWeatherByCity: builder.query({
-            query: (city) => `weather?q=${city}&appid=${API_KEY}&units=metric`,
-            // query: (city) => `weather?q=${city}`,
+            query: (city) => `weather?q=${city}${myHeader}`,
         }),
         getWeatherByCoord: builder.query({
-            query: ({lat, lon}) =>
-                `forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`,
-            // query: ({lat, lon}) =>
-            // `forecast?lat=${lat}&lon=${lon}`,
+            query: ({lat, lon}) => `forecast?lat=${lat}&lon=${lon}${myHeader}`,
         }),
     }),
 });
